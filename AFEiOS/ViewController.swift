66b9f14d1ddc502698ca9278ee5a363dfe577bad
nirav-tukadiya <nirav.tukadiya@gmail.com>
2019-06-24 16:27:03 +0530

    Added validation for inputs fields

--- AFEiOS/ViewController.swift
+++ AFEiOS/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var bSend: UIButton!
+    @IBOutlet weak var lResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func validateInputs(_ sender: Any) {
+        let (first, second) = isInputValid()
+        if(first != 0 && second != 0){
+            sendDataToFlutterModule(first: first, second: second)
+        }
+    }
+    
+    func sendDataToFlutterModule(first: Int, second: Int) {
+        // TODO will be implemented later
+    }
+    
+    func isInputValid() -> (Int, Int){
+        
+        let sFirst = firstNumber.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        let sSecond = secondNumber.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        if(sFirst.isEmpty){
+            showMessage(msg: "Please enter first number")
+        }else if(sSecond.isEmpty){
+            showMessage(msg: "Please enter second number")
+        }else{
+            return (Int(sFirst)!, Int(sSecond)!)
+        }
+        return (0,0)
+    }
+    
+    func showMessage(msg: String){
+        let alert = UIAlertController(title: "", message: msg, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
 
