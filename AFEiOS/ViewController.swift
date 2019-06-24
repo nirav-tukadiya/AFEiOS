3afd6ca16f6d3953c0603fd64d695977d1105a3f
nirav-tukadiya <nirav.tukadiya@gmail.com>
2019-06-24 14:12:06 +0530

    Project setup and UI design

---
+++ AFEiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AFEiOS
+//
+//  Created by NIRAV on 24/06/19.
+//  Copyright © 2019 NIRAV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
